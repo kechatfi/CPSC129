591f2e000976bdd19b66b738ac86d41c6a012b18
kechatfi <33097882+kechatfi@users.noreply.github.com>
2024-10-09 12:03:43 -0700

    Remove Site

--- site/09.2_Data_Processing/05_exceptions.py
+++
@@ -1,8 +0,0 @@
-done = False
-while not done:
-    try:
-        x = int(raw_input('Please enter an integer value: '))
-        done = True
-    except ValueError:
-        print 'Give me an integer damnit!'
-print 'Thanks!'
